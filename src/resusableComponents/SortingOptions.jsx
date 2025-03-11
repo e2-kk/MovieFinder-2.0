544aeb6eaf953fc4705e6abad0a3cce6ac08695d
e2-kk <75497489+e2-kk@users.noreply.github.com>
2025-03-11 10:06:01 +0000

    changed name values inside select tags for movies sorting options

--- src/resusableComponents/SortingOptions.jsx
+++ src/resusableComponents/SortingOptions.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./SortingOptions.css";
 
 const SortingOptions = () => {
+  const [sortingOption, setSortingOption] = useState({
+    year: "release_date",
+    rate: "rating",
+    services: "watch_providers",
+  });
   return (
     <div className="sorting-options container">
       <select
         className="option"
-        name="release_date"
+        name="year"
         //onChange
         //value
       >
@@ -17,24 +22,24 @@ const SortingOptions = () => {
       </select>
       <select
         className="option"
-        name="release_date"
+        name="rate"
         //onChange
         //value
       >
-        <option value="release_date">Rating</option>
-        <option value="release_date_asc">Ascending</option>
-        <option value="release_date_desc">Descending</option>
+        <option value="rating">Rating</option>
+        <option value="rating_asc">Ascending</option>
+        <option value="rating_desc">Descending</option>
       </select>
       <select
         className="option"
-        name="watch_providers"
+        name="services"
         //onChange
         //value
       >
-        <option value="release_date">Services</option>
-        <option value="release_date">Netflix</option>
-        <option value="release_date_asc">Apple TV</option>
-        <option value="release_date_desc">YouTube</option>
+        <option value="watch_providers">Services</option>
+        <option value="Netflix">Netflix</option>
+        <option value="Apple TV">Apple TV</option>
+        <option value="YouTube">YouTube</option>
       </select>
     </div>
   );
