7d07c19f8dd28cebb45c0271c139409378a5d395
e2-kk <75497489+e2-kk@users.noreply.github.com>
2025-04-17 13:58:43 +0100

    added mobile menu

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -12,6 +12,7 @@ const NavBar = ({
   setSotringOption,
 }) => {
   const [activeLink, setActiveLink] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     const fetchCategories = async () => {
       const data = await getMoviesCategories();
@@ -26,13 +27,69 @@ const NavBar = ({
     setActiveLink("");
   };
 
+  const openMobileMenu = () => {
+    setIsOpen(true);
+  };
+
+  const closeMobileMenu = () => {
+    setIsOpen(false);
+  };
   return (
     <section className="section-nav">
       <div className="section-nav-container">
         <nav className="nav">
           <h1 className="nav-logo">MovieFinder</h1>
-          <ul className="nav-list">
+          <ul className="nav-list-desktop">
             <li className="nav-list-link">
+              <a href="/">Films</a>
+            </li>
+            <li className="nav-list-link">
+              <a href="#">Genres</a>
+              <ul className="genres-dropdown">
+                {moviesCategories.map((category) => (
+                  <li
+                    className={activeLink === category.name ? "active" : ""}
+                    key={category.id}
+                    onClick={() => {
+                      setSelectedCategory(category.id);
+                      setMovies([]);
+                      setActiveLink(category.name);
+                      setSotringOption({
+                        year: "release_date",
+                        rate: "rating",
+                        services: "watch_providers",
+                      });
+                      navigate("/", { state: { category } });
+                    }}
+                  >
+                    {category.name}
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li className="nav-list-link">
+              <Link to="/watch-list" onClick={handleSelectedCategoryReset}>
+                Watch List
+              </Link>
+            </li>
+          </ul>
+          <li className="nav-list-mobile-menu-btn">
+            <img
+              src="/assets/menu.svg"
+              alt="menu"
+              onClick={openMobileMenu}
+            ></img>
+          </li>
+          <ul className={`nav-list-mobile ${isOpen ? "is-open" : ""}`}>
+            <li>
+              <img
+                src="/assets/close.svg"
+                alt="close button"
+                className="nav-list-mobile-close-btn"
+                onClick={closeMobileMenu}
+              ></img>
+            </li>
+            <li className="nav-list-link margin-top">
               <a href="/">Films</a>
             </li>
             <li className="nav-list-link">
